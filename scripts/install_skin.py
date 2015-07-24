fface8ed51f9cdaec74fe83aba1c11aa7fe6e452
Dorel <idorel@gmail.com>
2015-07-24 15:42:24 +0300

    Update install_skin.py

--- scripts/install_skin.py
+++ scripts/install_skin.py
@@ -4,11 +4,13 @@ import os
 import socket
 
 
-xbmcgui.Dialog().notification('Skin', 'Start skin Update', xbmcgui.NOTIFICATION_INFO, 5000)
+xbmcgui.Dialog().notification('Skin', 'Start skin Update', 'Please wait ...', xbmcgui.NOTIFICATION_INFO, 5000)
 os.system("sudo rm -r /home/pi/skin.CarPC-touch_carbon/");
+xbmcgui.Dialog().notification('Skin', 'Download Update...', xbmcgui.NOTIFICATION_INFO, 5000)
 os.system("git clone https://github.com/idorel77/skin.CarPC-touch_carbon");
+xbmcgui.Dialog().notification('Skin', 'Install Update...', xbmcgui.NOTIFICATION_INFO, 5000)
 os.system("sudo rm -r /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
 os.system("sudo mkdir /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
 os.system("sudo rsync -r skin.CarPC-touch_carbon/ /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
-xbmcgui.Dialog().notification('Skin', 'Skin Update Done', xbmcgui.NOTIFICATION_INFO, 5000)
+xbmcgui.Dialog().notification('Skin', 'Skin Updated', xbmcgui.NOTIFICATION_INFO, 5000)
 xbmc.executebuiltin('ReloadSkin()');
