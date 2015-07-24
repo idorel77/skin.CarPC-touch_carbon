d304a386a8bf95228ad5e6056a60ca234b3e7eec
Dorel <idorel@gmail.com>
2015-07-24 14:22:08 +0300

    Update and rename dowload_skin.py to install_skin.py

--- scripts/dowload_skin.py
+++
@@ -1,5 +0,0 @@
-import os
-import xbmc
-
-
-os.system("git clone https://github.com/idorel77/skin.CarPC-touch_carbon.git");
---
+++ scripts/install_skin.py
@@ -0,0 +1,9 @@
+import os
+import xbmc
+
+
+os.system("git clone https://github.com/idorel77/skin.CarPC-touch_carbon");
+print "Download done"
+os.system("rsync -avz -e skin.CarPC-touch_carbon/ /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
+print "Sync done"
+os.system("sudo reboot");
