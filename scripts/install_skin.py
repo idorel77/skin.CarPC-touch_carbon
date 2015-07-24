f02eb55d2f8cb846937529f976ee9be17ad2d030
Dorel <idorel@gmail.com>
2015-07-24 15:22:19 +0300

    Update install_skin.py

--- scripts/install_skin.py
+++ scripts/install_skin.py
@@ -1,11 +1,12 @@
+import xbmcgui
+import time
 import os
-import xbmc
+import socket
 
 os.system("sudo rm -r /home/pi/skin.CarPC-touch_carbon/");
 os.system("git clone https://github.com/idorel77/skin.CarPC-touch_carbon");
-    print "Download done"
 os.system("sudo rm -r /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
 os.system("sudo mkdir /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
 os.system("sudo rsync -r skin.CarPC-touch_carbon/ /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
-    print "Sync done"
+xbmcgui.Dialog().notification('Skin', 'Skin Updated', xbmcgui.NOTIFICATION_INFO, 5000)
 xbmc.executebuiltin('ReloadSkin()');
